8f890b1bc6be3224da601541005ede0f8e38f39c
fuhui <fuhui@luojilab.com>
2020-04-28 09:00:52 +0800

    引入css、js文件

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
+	router.Static("/resource", "./resource")
 	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+		c.HTML(http.StatusOK, "default.tmpl", gin.H{
 			"title": "Main website",
 		})
 	})
