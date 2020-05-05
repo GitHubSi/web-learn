9252a21ec1430ea94ce86a16abd75ba04a55c6bb
fuhui <fuhui@luojilab.com>
2020-05-05 22:29:10 +0800

    追加了异步Get请求的方法

--- main.go
+++ main.go
@@ -10,9 +10,30 @@ func main() {
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/resource", "./resource")
 	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "default.tmpl", gin.H{
-			"title": "Main website",
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"templateTitle": "card-list.tmpl",
 		})
+	})
+
+	router.GET("/get-request", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"templateTitle": "get-request.tmpl",
+		})
+	})
+
+	router.POST("/set-progress", func(context *gin.Context) {
+		type Request struct {
+			Progress string `json:"progress" form:"progress"`
+		}
+
+		request := &Request{}
+		if err := context.Bind(request); err != nil {
+		}
+
+		context.JSON(http.StatusOK, gin.H{
+			"progress": request.Progress,
+		})
+		return
 	})
 
 	router.Run(":8080")
